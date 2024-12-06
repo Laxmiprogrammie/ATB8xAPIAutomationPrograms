7e47519086da977a13c5ec2de3316444d5237832
Laxmiprogrammie <ramlakshmi.9177@gmail.com>
2024-12-06 15:25:52 +0530

    29th api

---
+++ src/test/java/comt/hetestingacademy/sample/check/APITest002.java
@@ -0,0 +1,17 @@
+package comt.hetestingacademy.sample.check;
+
+import io.restassured.RestAssured;
+
+public class APITest002 {
+    public static void main(String[] args) {
+        RestAssured
+                .given()
+                   .baseUri("https://restful-booker.herokuapp.com/")
+                    .basePath("/booking/1")
+                .when()
+                   .get()
+                .then().log().all()
+                    .statusCode(200);
+
+    }
+}
